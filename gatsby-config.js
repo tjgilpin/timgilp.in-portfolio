4a3e9578fe00c28b010185347b5859ea96c84761
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2018-10-04 15:51:44 +0100

    Add offline, manifest, icon.png and google analytics plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,19 @@ module.exports = {
       }
     },
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tim Gilpin UI/UX Portfolio`,
+        short_name: `Tim Gilpin`,
+        start_url: `/`,
+        background_color: `#14191e`,
+        theme_color: `#cccccc`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ]
 };
