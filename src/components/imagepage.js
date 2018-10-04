cbc2f4f6345c2fb1b646ba4f9f586889e202ed39
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2018-10-04 13:48:20 +0100

    CSS tweaks - make button a lil bit bigger

--- src/components/imagepage.js
+++ src/components/imagepage.js
@@ -9,7 +9,7 @@ const ImagePage = ({ linkage, imgsize, wrapperstyle }) => (
     <Helmet bodyAttributes={{style: "background: #000" }} />
     <div style={{display: 'block'}}>
       <Link to={linkage} replace>
-        <span style={{ position: "fixed", right: "10px", top: "10px", zIndex: "100", fontSize: "1.75rem", fontWeight: "900", background: "rgba(0, 0, 0, 0.8)", color: "rgb(255, 255, 255)", width: "2rem", height: "2rem", textAlign: "center", borderRadius: "50%", display: "inline-flex", justifyContent: "center", alignItems: "center" }}>×</span>
+        <span style={{ position: "fixed", right: "10px", top: "10px", zIndex: "100", fontSize: "1.85rem", fontWeight: "300", background: "rgba(0, 0, 0, 0.8)", color: "rgb(255, 255, 255)", width: "2.5rem", height: "2.5rem", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center" }}>×</span>
         <div style={{ maxWidth: wrapperstyle + 'px', margin: '0 auto', position: 'relative' }}>
           <Img fluid={imgsize} alt={`Recordstore`} />
         </div>   
