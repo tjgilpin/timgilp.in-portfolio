bf76ef23ba56ea53ed526c03f935ada231d50598
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2018-10-11 11:01:47 +0100

    Refactor helmet code and add meta description

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,14 @@ import Helmet from "react-helmet"
 
 export default ({ children }) => (
     <React.Fragment>
-      <Helmet title="Tim Gilpin UI/UX Design Portfolio" />
-      <Helmet htmlAttributes={{ lang: "en" }} />
+      <Helmet>
+        <title>Tim Gilpin UI/UX Design Portfolio</title>
+        <html lang="en" />
+        <meta
+          name="description"
+          content="Tim Gilpin UI/UX Design Portfolio - Recordstore, Brentwood Benson, Universal Music Group, The Beatles, Worship Charts and Massive Attack"
+        />
+      </Helmet>
       {children}
     </React.Fragment>
   )
