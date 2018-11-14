f6b320ced58c32c6b741cf63db2c862d9ecd913d
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2018-11-14 11:20:51 +0000

    Add google site verification for search console linking

--- src/html.js
+++ src/html.js
@@ -17,6 +17,7 @@ export default class HTML extends React.Component {
             name="description"
             content="Tim Gilpin UI/UX Design Portfolio - Recordstore, Brentwood Benson, Universal Music Group, The Beatles, Worship Charts and Massive Attack"
           />
+          <meta name="google-site-verification" content="VPC2qUVCEE02Lz9xekJ_qjLPNn54QW0ICSVLzlTfd6c" />
           {this.props.headComponents}
         </head>
         <body className="ma0 _add-bg-dark-metallic-blue _add-font-roboto" {...this.props.bodyAttributes}>
